4f1b6894cc325c9c7b3fe3858bf1608dbc7587b4
sarwar3w <sarwar@triplewsols.org>
2023-09-01 20:00:53 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import YtCard from "./components/YtCard/YtCard";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-start bg-white dark:bg-slate-900">
+      <YtCard />
+      <YtCard />
+      <YtCard />
+      <YtCard />
+      <YtCard />
+      <YtCard />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/YtCard/YtCard.jsx
@@ -0,0 +1,36 @@
+import thumbnail from "../../assets/thumbnail.jpg";
+
+const YtCard = () => {
+  return (
+    <div className='bg-white dark:bg-slate-900 text-white flex flex-col'>
+      <div className='w-full rounded-lg'>
+        <img src={thumbnail} alt='thumbnaail' className='w-full rounded-lg' />
+      </div>
+      <div className='text-white p-2 flex gap-4 my-4'>
+        <div>
+          <img
+            src='https://randomuser.me/api/portraits/men/32.jpg'
+                      alt='demo-user'
+                      className="w-12 rounded-full"
+          />
+        </div>
+              <div>
+                  <h3 className="text-white text-2xl">Demo Title</h3>
+                  <p className="text-slate-500">channel Name</p>
+                  <div className="flex gap-3">
+                      <span>
+                          1k views
+                      </span>
+                      <span>.</span>
+                      <span>
+                          2 minutes aga
+                      </span>
+                  </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YtCard;
